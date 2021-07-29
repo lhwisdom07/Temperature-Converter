a455a874ea1e94c485524ca9183ec137385aa1bf
lhwisdom07 <87719255+lhwisdom07@users.noreply.github.com>
2021-07-29 07:11:28 -0500

    Add files via upload

---
+++ Temperature _Converter.py
@@ -0,0 +1,9 @@
+fahrenheit = eval(input("Enter the temperature in Fahrenheit: "))
+tempInC =  (fahrenheit - 32) * 5/9                  
+print( "The temperature in Celsius is", tempInC, "degrees" )
+fahrenheit = eval(input("Enter the temperature in Fahrenheit: "))
+tempInC =  (fahrenheit - 32) * 5/9                  
+print( "The temperature in Celsius is", tempInC, "degrees" )
+fahrenheit = eval(input("Enter the temperature in Fahrenheit: "))
+tempInC =  (fahrenheit - 32) * 5/9                  
+print( "The temperature in Celsius is", tempInC, "degrees" )
